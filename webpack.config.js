242e0386ba50c044b781884848627b93b3f5b823
Dj_Vreditel <Dj_Vreditel@mail.ru>
2020-09-07 22:27:58 +0300

    Update master

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+
+module.exports = {
+  entry: './app.js',
+  output: {
+    path: path.join(__dirname, '/'),
+  },
+
+  module: {
+    rules: [{
+      test: /\.scss$/,
+      use: ExtractTextPlugin.extract({
+        fallback: "style-loader",
+        use: [{loader: 'css-loader'}, {loader: 'resolve-url-loader'}, {loader: 'sass-loader'}],
+      })
+    }, {
+      test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+      use: {
+        loader: 'url-loader'
+      },
+    }]
+  },
+  plugins: [
+    new ExtractTextPlugin({
+      filename: (getPath) => getPath('bundle/styles.css').replace('bundle', 'css')
+    }),
+    new BrowserSyncPlugin({
+      watch: true,
+      host: 'localhost',
+      port: 3000,
+      server: {baseDir: ['./']}
+    })
+  ],
+  optimization: {
+    minimize: true,
+    minimizer: [new UglifyJsPlugin({
+      include: /\.min\.js$/
+    })]
+  }
+};
